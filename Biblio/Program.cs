798c967981c77351fdd0e7ccf6c064154a9222c3
alebizzo <44555133+alebizzo@users.noreply.github.com>
2021-01-15 12:13:29 +0100

    Biblio create

---
+++ Biblio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Biblio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Utente
+	{
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string MyProperty { get; set; }
+
+	}
+}
